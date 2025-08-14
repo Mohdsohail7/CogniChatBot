a9d67618e3557044b6e56cbe971067102b88c83b
Mohdsohail7 <sohailmohd437@gmail.com>
2025-08-14 18:46:50 +0530

    feat:frontend started with tailwind css

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-italic text-color-blue">
+      Hello world!
+    </h1>
+    </>
+  );
+}
+
+export default App;
